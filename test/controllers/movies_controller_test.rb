4627fde1684a9af39596991dcc12e5eb09ca64c4
ticho <tibofolly@gmail.com>
2018-08-09 22:45:47 +0200

    test

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -2,8 +2,18 @@ require 'test_helper'
 
 class MoviesControllerTest < ActionDispatch::IntegrationTest
   test "should get search" do
-    get movies_search_url
+    get "/"
     assert_response :success
   end
 
+  test "should return a table for mononoke" do
+    post search_url, params: { movie_name: 'la ligne verte' }
+    assert_select 'table'
+    assert_select 'td', 'The Green Mile'
+  end
+
+  test "should return a sorry msg if nothing is found" do
+    post search_url, params: { movie_name: 'aaaaaaaaaaaaaaaaa' }
+    assert_select 'h3', 'Sorry, no result for this query.'
+  end
 end
